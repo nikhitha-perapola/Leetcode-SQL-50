8c17e2034658615aa7274a3f6357ae43a53d2d64
Nikhitha Perapola <63772752+nikhitha-perapola@users.noreply.github.com>
2023-10-10 13:19:01 +0530

    Create 197. Rising Temperature.sql

---
+++ Basic_Joins/197. Rising Temperature.sql
@@ -0,0 +1,24 @@
+'''
+SQL Schema
+Pandas Schema
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the column with unique values for this table.
+This table contains information about the temperature on a certain day.
+ 
+
+Write a solution to find all dates Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+'''
+#code:
+SELECT w1.id
+FROM Weather w1, Weather w2
+WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature;
